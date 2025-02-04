68c4cfe5d36560e39a48654a977b6beb7fb2218d
Vishnu Kadaba <vishnuswarupkadaba@gmail.com>
2025-02-04 15:27:41 -0800

    C++ for extra pts works

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -10,19 +10,24 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
 	// check to see if user input is valid
 	char socket_read_buffer[1024];
 
 	// TODO: Fill out the server ip and port
-	std::string server_ip = "";
-	std::string server_port = "";
+	std::string server_ip = "172.20.10.13";
+	std::string server_port = "8080";
+
+	// cout << "alive" << endl;
 
 	int opt = 1;
 	int client_fd = -1;
 
 	// TODO: Create a TCP socket()
+	client_fd = socket(AF_INET, SOCK_STREAM, 0);
 
 	// Enable reusing address and port
 	if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
@@ -46,11 +51,34 @@ int main(int argc, char const *argv[])
 	getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &server_addr);
 
 	// TODO: Connect() to the server (hint: you'll need to use server_addr)
-
+	int n = connect(client_fd, server_addr->ai_addr, server_addr->ai_addrlen);
+	if (n == -1)
+	{
+		perror("bad connect");
+		exit(1337);
+	}
 	// TODO: Retreive user input
+	// char message[256];
+	// cin >> message;
+
+	cout << "Enter  message: ";
+	cin >> socket_read_buffer;
+
 	// TODO: Send() the user input to the server
+	int b = write(client_fd, socket_read_buffer, 256);
 	// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
+	b = read(client_fd, socket_read_buffer, 1024);
+	cout << "read" << endl;
+	if (b == -1)
+	{
+		cout << "h" << endl;
+		perror("bad read from server");
+		exit(1337);
+	}
+	cout << "server: " << socket_read_buffer << endl;
+
 	// TODO: Close() the socket
+	close(client_fd);
 
 	return 0;
 }
